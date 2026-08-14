e7aa12481db9b739a015b55ebf5324b2419ad3c9
teja <tejalakhinana@gmail.com>
2026-08-14 18:14:35 +0530

    Initial C++ setup

---
+++ day01.cpp
@@ -0,0 +1,10 @@
+//#include<bits/stdc++.h>  - this is used to import all the libraries in c++
+#include<iostream>
+using namespace std;    //without this, we need to use std on every function.
+int main(){
+    cout << "Hello World!" << endl;      //It moves the cursor to the next line after printing.
+    cout << "This is my first C++ code.";
+    int x, y;           //we can declare the variables at time using ,
+    cin >> x >> y;      //to take the input of two declared variables in a single line.
+    cout << "The value of x is: " << x << " and y is: "  << y;
+}
